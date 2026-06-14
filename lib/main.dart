be01af1e3212cc238449ae53b122e3b8d3f2e113
cw628310 <1032145332@qq.com>
2026-06-14 16:14:09 +0800

    创建 lib/main.dart

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import 'pages/home_page.dart';
+
+void main() {
+  runApp(const DataAnalyzerApp());
+}
+
+class DataAnalyzerApp extends StatelessWidget {
+  const DataAnalyzerApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '数据分析仪',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF2563EB)),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
